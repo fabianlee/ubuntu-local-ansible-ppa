78d86ac1f59cbf3875fd28a5c3751fecc996afe5
Fabian Lee <flee999@hotmail.com>
2021-01-02 17:21:43 +0000

    support password required in sudo

--- 3-install-localppa.sh
+++ 3-install-localppa.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ansible-playbook install-ppa.yml
+sudopw="$1"
+
+if [ -z $sudopw ]; then
+  ansible-playbook install-ppa.yml
+else
+  ansible-playbook install-ppa.yml --extra-vars "ansible_sudo_pass=$sudopw"
+fi
 
 # if you wanted to invoke using group
 #ansible-playbook install-ppa.yml -l ansibleppa
