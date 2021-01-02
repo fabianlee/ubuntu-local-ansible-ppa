c541790e1b21d396ef57ca8f86c79d66adcd6fc7
Fabian Lee <flee999@hotmail.com>
2021-01-02 22:14:36 +0000

    ping with module and playbook

--- 1-ansible-ping.sh
+++ 1-ansible-ping.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# ping using module
+echo "Doing ansible ping with module"
 ansible localhost -m ping
+
+echo ""
+
+# ping using playbook
+echo "Doing ansible ping with playbook"
+ansible-playbook playbook-ping.yml
